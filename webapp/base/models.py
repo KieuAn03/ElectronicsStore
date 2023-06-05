1a296a63c3482f0682dc8772703a0cdcb9e46adc
Mạc An Kiều <ankieu17@gmail.com>
2023-06-05 20:20:03 +0700

    Cập nhật Models: Thêm Phone, Phonebrand, PhoneScreen,...
    Thêm Models:
    Phone, Phone Brand, PhoneOS
    Vì các thuộc tính của điện thoại đa phần có sự sử dụng lại, nên tạo 1 bảng chứa các thuộc tính đó sẽ giúp dễ dàng phát triển cũng như quản lý

--- webapp/base/models.py
+++ webapp/base/models.py
@@ -1,10 +1,37 @@
 from django.db import models
 
 # Create your models here.
+class PhoneOS(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    def __str__ (self):
+        return self.name
+
+class PhoneBrand(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    def __str__ (self):
+        return self.name
+class PhoneScreen(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    def __str__ (self):
+        return self.name
 class Product (models.Model):
     id = models.AutoField(primary_key=True)
+    image = models.ImageField()
     Name = models.CharField(max_length=100)
     price = models.IntegerField()
     stock = models.IntegerField()
+    def __str__ (self):
+        return self.Name 
+
+
+class Phone (models.Model):
+    id = models.AutoField(primary_key=True)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    screen = models.ForeignKey(PhoneScreen, on_delete=models.CASCADE)
+    OS = models.ForeignKey(PhoneOS, on_delete=models.CASCADE)
+    brand = models.ForeignKey(PhoneBrand, on_delete=models.CASCADE)
     def __str__ (self):
         return self.Name
