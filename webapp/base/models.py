8392db4ddb9f340526e9d328813064e1a01627a0
Mạc An Kiều <ankieu17@gmail.com>
2023-06-05 22:12:59 +0700

    sửa models :Thêm product type để phân loại các danh mục sp

--- webapp/base/models.py
+++ webapp/base/models.py
@@ -1,6 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductType(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    def __str__ (self):
+        return self.name
 class PhoneOS(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
@@ -12,13 +17,16 @@ class PhoneBrand(models.Model):
     name = models.CharField(max_length=100)
     def __str__ (self):
         return self.name
+
 class PhoneScreen(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
     def __str__ (self):
         return self.name
+    
 class Product (models.Model):
     id = models.AutoField(primary_key=True)
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE)
     image = models.ImageField()
     Name = models.CharField(max_length=100)
     price = models.IntegerField()
