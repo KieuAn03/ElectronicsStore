f8fcb7ae8800a964d6ba2b4c1b8d80850d3fcc8d
Mạc An Kiều <ankieu17@gmail.com>
2023-06-08 17:00:49 +0700

    Tạo models cartitem

--- webapp/cart/models.py
+++ webapp/cart/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from base.models import *
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Cart_Item(models.Model):
+    user = models.ForeignKey('User', on_delete=models.CASCADE)
+    product = models.ForeignKey('Product', on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    time_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return User.username + ':' + Product.name + '-sl:' + str(self.quantity)
+
+
+
