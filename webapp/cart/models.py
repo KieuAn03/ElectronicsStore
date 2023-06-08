e5ac6e16cb3e3574d713f48f8cbf89cf3ef4fb46
Mạc An Kiều <ankieu17@gmail.com>
2023-06-08 17:04:01 +0700

    Thêm model cart và cập nhật lại cartItems

--- webapp/cart/models.py
+++ webapp/cart/models.py
@@ -2,13 +2,22 @@ from django.db import models
 from base.models import *
 from django.contrib.auth.models import User
 
+class Cart(models.Model):
+    user = models.OneToOneField('User', on_delete=models.CASCADE)
+    time_created = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.user.username + ':' + str(self.time_created)
+
 class Cart_Item(models.Model):
     user = models.ForeignKey('User', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', on_delete=models.CASCADE, null=True)
     product = models.ForeignKey('Product', on_delete=models.CASCADE)
     quantity = models.IntegerField(default=1)
     time_created = models.DateTimeField(auto_now_add=True)
     def __str__(self):
         return User.username + ':' + Product.name + '-sl:' + str(self.quantity)
+
+
 
 
 
