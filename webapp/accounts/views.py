aabdc89d2102bd9dce94c09a39768bdac49c6fe4
pnQuanq <phamquang13103@gmail.com>
2023-06-09 12:24:21 +0700

    login function

--- webapp/accounts/views.py
+++ webapp/accounts/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib import messages      
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate , login , logout
+from django.http import HttpResponseRedirect,HttpResponse
+
+from products.models import *
 
 # Create your views here.
+def login_page(request):
+    
+    if request.method == 'POST':
+        username = request.POST.get('userName')
+        password = request.POST.get('password')
+        user_obj = User.objects.filter(username = username)
+
+        if not user_obj.exists():
+            messages.warning(request, 'Account not found.')
+            return HttpResponseRedirect(request.path_info)
+
+
+
+        user_obj = authenticate(username = username , password= password)
+        if user_obj:
+            login(request , user_obj)
+            return redirect('/')
+
+        
+
+        messages.warning(request, 'Invalid credentials')
+        return HttpResponseRedirect(request.path_info)
+
+    return render(request ,'accounts/login.html')
