0813ca231750a98859e9413415cb79576c798072
Mạc An Kiều <ankieu17@gmail.com>
2023-06-21 16:34:22 +0700

    tạo link liên kết giữa các sản phẩm đến backend

--- webapp/home/views.py
+++ webapp/home/views.py
@@ -11,7 +11,26 @@ def index(request):
 
     return render(request , 'home/index.html',context)
 
-def details(request):
-    return render(request , 'detail.html')
 def checkouts(request):
     return render(request, 'checkout.html')
+def deltail(request):
+    """
+    hàm lấy dữ liệu thể loại, tên truyện, số chương, chương truyện, để đưa lên web
+    """
+    id = request.GET.get('id','') 
+    truyens = truyen.objects.filter(id=id) 
+    tr = truyen.objects.get(id=id)
+    tr.view_count=tr.view_count+1
+    tr.save()   
+    ct = truyen_category.objects.filter(Truyen = id)
+    chapters = chapter.objects.filter(truyen = id)
+    num_chap = chapter.objects.filter(truyen = id).count()
+    num_cmt = comment.objects.filter(truyen=id).count()
+    context = {
+        'truyens' : truyens,
+        'cts': ct,
+        'chapters' : chapters,
+        'num_cmt' : num_cmt,
+        'num_chap' : num_chap,
+    }
+    return render(request, 'detail.html',context)
