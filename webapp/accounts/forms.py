3e0ddbd43aaaa91066c4bed22b1c45fd170e106c
pnQuanq <phamquang13103@gmail.com>
2023-07-03 23:28:31 +0700

    lỗi đăng nhập tk không thành công

--- webapp/accounts/forms.py
+++ webapp/accounts/forms.py
@@ -2,7 +2,23 @@ from django import forms
 from django.contrib.auth.models import User
 from .models import Profile
 from django.forms.widgets import FileInput
+from django.contrib.auth.forms import UserCreationForm
+from django.forms.models import ModelForm
 
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = '__all__'
+        exclude = ['user']
+        widgets = {
+         'profile_img': FileInput(),
+         }
 class UserUpdateForm(forms.ModelForm):
     email = forms.EmailField()
 
--- webapp/accounts/views.py
+++ webapp/accounts/views.py
@@ -3,54 +3,64 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate , login , logout
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponseRedirect
-from .forms import UserUpdateForm, ProfileUpdateForm
-from .models import Profile
+from .forms import UserUpdateForm, ProfileUpdateForm, UserRegisterForm, ProfileForm
+from .decorators import unauthenticated_user
 
 # Create your views here.
-def login_page(request):
-    
+@login_required(login_url='login')
+def profile(request):
     if request.method == 'POST':
-        username = request.POST.get('userName')
-        password = request.POST.get('password')
-        user_obj = User.objects.filter(username = username)
-
-        if not user_obj.exists():
-            messages.warning(request, 'Account not found.')
-            return HttpResponseRedirect(request.path_info)
-
-
-
-        user_obj = authenticate(username = username , password= password)
-        if user_obj:
-            login(request , user_obj)
+        form = ProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if form.is_valid():
+            form.save()
+            username = request.user.username
+            messages.success(request, f'{username}, Your profile is updated.')
             return redirect('/')
+    else:
+        form = ProfileForm(instance=request.user.profile)
+    context = {'form':form}
+    return render(request, 'profileapp/profile.html', context)
 
-        
+@unauthenticated_user
+def login_page(request):
 
-        messages.warning(request, 'Invalid credentials')
-        return HttpResponseRedirect(request.path_info)
-
-    return render(request ,'accounts/login.html')
-
-def register_page(request):
-    if request.method=='POST':
-        user_name = request.POST.get('userName')
-        email = request.POST.get('email')
-        firstName = request.POST.get('first_name')
-        lastName = request.POST.get('last_name')
+    if request.method == 'POST':
+        username = request.POST.get('username')
         password = request.POST.get('password')
-        re_pass = request.POST.get('rePassword')
 
-        if password != re_pass:
-            messages.warning(request, 'Coupon already exists.')
-            return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.info(request, f'{username}, You are logged in.')
+            return redirect("/")
         else:
-            user = User.objects.create_user(username = user_name , email = email)
-            user.set_password(password)
-            user.save()
-            return redirect('index')
-    return render(request ,'accounts/register.html')
+            messages.info(request, 'Wrong passwrod or username')
+            return redirect('login')
+    return render(request, 'accounts/login.html')
+
+@unauthenticated_user
+def register_page(request):
+    form = UserRegisterForm()
+
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.info(request, 'Account is created.')
+            return redirect('login')
+        else:
+            context = {'form': form}
+            messages.info(request, 'Invalid credentials')
+            return render(request, 'accounts/register.html', context)
+
+    context = {'form': form}
+    return render(request, 'accounts/register.html', context)
+
+@login_required(login_url='login')
+def logout_user(request):
+    logout(request)
+    messages.info(request, 'You logged out successfully')
+    return redirect('login')
 
 def profile_page(request):
     form = ProfileUpdateForm()
@@ -84,3 +94,6 @@ def update_profile_page(request):
         'p_form': p_form
     }
     return render(request, 'accounts/update_profile.html', context)
+
+def logout_page(request):
+    return render(request, 'accounts/logout.html')
