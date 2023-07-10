6fbfefc9fc019f834be835346e8069843704b6e3
Mạc An Kiều <ankieu17@gmail.com>
2023-07-10 23:50:08 +0700

    Cho phép thêm sản phẩm điện thoại vào cart với các tùy chọn về cấu hình , màu sắc

--- webapp/cart/urls.py
+++ webapp/cart/urls.py
@@ -4,5 +4,11 @@ from cart.views import *
 urlpatterns = [
     path("", views.index),
     path('remove-cart/<cart_item_uid>/' , remove_cart, name="remove_cart"),
+    
+    
+    path('add-to-cart/<id>/' , add_to_cart, name="add_to_cart"),
+    path('add-to-cart/<id>/<color>/' , add_to_cart, name="add_to_cart"),
     path('add-to-cart/<id>/<ram>/<storage>/' , add_to_cart, name="add_to_cart"),
+    path('add-to-cart/<id>/<ram>/<storage>/<color>/' , add_to_cart, name="add_to_cart"),
+    
 ]
--- webapp/cart/views.py
+++ webapp/cart/views.py
@@ -13,19 +13,44 @@ def index(request):
     }
     return render(request, 'cart.html',context)
 
-def add_to_cart(request , uid):
-    variant = request.GET.get('variant')
-    product = Product.objects.get(uid = uid)
+def add_to_cart(request , **kwargs):
+    product = Product.objects.get(id = kwargs.get('id'))
     user = request.user
-    cart ,  _ = Cart.objects.get_or_create(user = user , is_paid = False)
-
-    cart_item = CartItems.objects.create(cart = cart , product = product , )
-
-    if variant:
-        variant = request.GET.get('variant')
-        size_variant = SizeVariant.objects.get(size_name = variant)
-        cart_item.size_variant = size_variant
-        cart_item.save()
+    cart ,  _= Cart.objects.get_or_create(user = user , complete = False)
+   
+    print(product.product_type) 
+    if(str(product.product_type) == 'Phone'):
+        
+        
+        phone = Phone.objects.get(product_id =  kwargs.get('id'))
+        if(kwargs.get('ram')):
+            print("THIS IS THE TYPEEE") 
+            customs = PhoneOptionHard.objects.all()
+            print(customs)
+            custom = PhoneOptionHard.objects.get(id=1)
+            for option in customs:
+                if(str(option.ram) == str(kwargs.get('ram')) and str(option.Storage) == str(kwargs.get('storage'))):
+                    custom = option
+                    break
+            if(kwargs.get('color')):  
+                print("IM GONNAA SAVEEEE")   
+                color = PhoneOptionColor.objects.get(color = kwargs.get('color'))    
+                cart_item = cart_item_phone.objects.create(cart=cart , product = phone , quantity = 1 ,price_add= custom,color = color)
+                cart_item.save()    
+                print("SAVVVVVVEEEE")
+            else:
+                color = PhoneOptionColor.objects.get(color = phone.color)    
+                cart_item = cart_item_phone.objects.create(cart=cart , product = phone , quantity = 1 ,price_add= custom,color = color)
+                cart_item.save()   
+        elif(kwargs.get('color')):
+            custom = PhoneOptionHard.objects.get(ram = phone.ram , storage = phone.storage)
+            cart_item = cart_item_phone.objects.create(cart = cart , phone = product ,quantity = 1, price_add= custom,color = kwargs.get('color'))
+            cart_item.save()
+        else:
+            custom = PhoneOptionHard.objects.get(ram = phone.ram , storage = phone.storage)
+            cart_item = cart_item_phone.objects.create(cart = cart , product = phone ,quantity = 1,price_add= custom, color = phone.color)
+            cart_item.save()
+        
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 
