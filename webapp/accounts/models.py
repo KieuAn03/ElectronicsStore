f3cfd5b337409c9e935db6e28510fdd54138341d
pnQuanq <phamquang13103@gmail.com>
2023-07-16 16:35:43 +0700

    fix lỗi db không tìm thấy table

--- webapp/accounts/models.py
+++ webapp/accounts/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User , on_delete=models.CASCADE)
-    profile_image = models.ImageField(upload_to='profile_images')
+    profile_image = models.ImageField(default='default.jpg',upload_to='profile_images')
     address = models.CharField(max_length=200, null=True)
     name = models.CharField(max_length=100,null=True)
     phone = models.CharField(max_length=100,null=True)
