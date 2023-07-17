39905094aa03fc34218e9c5fa7f8901c0339ee00
pnQuanq <phamquang13103@gmail.com>
2023-07-17 11:48:12 +0700

    sửa lại link nút cart

--- webapp/manager/views.py
+++ webapp/manager/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from home.models import Product
 # Create your views here.
 def revenue_static(request):
     return render(request, 'manager/revenue-static.html')
