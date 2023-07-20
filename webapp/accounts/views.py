18bdf41c6129f7cc0f46e84871e8e00da35219ac
pnQuanq <phamquang13103@gmail.com>
2023-07-20 17:56:09 +0700

    thay đổi đường dẫn đăng xuất

--- webapp/accounts/views.py
+++ webapp/accounts/views.py
@@ -94,7 +94,7 @@ def update_profile_page(request):
 
 def logout_page(request):
     logout(request)
-    return render(request, 'accounts/logout.html')
+    return render(request, 'accounts/login.html')
 def remove_coupon(request, cart_id):
     cart = Cart.objects.get(uid = cart_id)
     cart.coupon = None
