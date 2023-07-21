41ea4fba87072f79f948b030f9b091d77e166ad7
pnQuanq <phamquang13103@gmail.com>
2023-07-21 21:47:18 +0700

    phân quyền truy cập ở các trang

--- webapp/staff/views.py
+++ webapp/staff/views.py
@@ -5,6 +5,7 @@ def staff_page(request):
     
     return render(request, 'staff/staff_page.html')
 
+
 def staff_profile(request,id_profile):
     #user = request.user
     staff_profile = StaffProfile.objects.get(id_profile= id_profile)
